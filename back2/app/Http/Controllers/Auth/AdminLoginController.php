76339450e0b638cf39d23d7bb851004e8422e6e6
Agnaldo <naoeodobyd@proton.me>
2025-10-18 12:49:59 -0300

    Update on Admin controller for better experience on login
    But the register don't work :/

--- back2/app/Http/Controllers/Auth/AdminLoginController.php
+++ back2/app/Http/Controllers/Auth/AdminLoginController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use App\Models\User;
 use Twilio\Rest\Client;
+use App\Http\Controllers\Controller;
 
 class AdminLoginController extends Controller
 {
